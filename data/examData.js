0ec35d9463657dbbd211b77d6fc58f734f32d4f3
mohitc9 <mohitchoudhary9m@gmail.com>
2025-06-24 03:11:35 +0530

    new

--- data/examData.js
+++ data/examData.js
@@ -24,7 +24,87 @@ module.exports = [
     sections: [
       { name: "Quant", timeLimit: 40 },
       { name: "VARC", timeLimit: 30 },
-      { name: "GA", timeLimit: 20 }
+      { name: "LRDI", timeLimit: 20 }
+    ]
+  },
+  {
+    name: "Common Management Admission Test",
+    code: "CMAT",
+    description: "MBA entrance by NTA",
+    quizCount: 40,
+    icon: "fas fa-briefcase",
+    colorTheme: "teal",
+    sections: [
+      { name: "Quant", timeLimit: 40 },
+      { name: "VARC", timeLimit: 30 },
+      { name: "LRDI", timeLimit: 30 }
+    ]
+  },
+  {
+    name: "NMAT by GMAC",
+    code: "NMAT",
+    description: "MBA entrance by GMAC",
+    quizCount: 40,
+    icon: "fas fa-university",
+    colorTheme: "blue",
+    sections: [
+      { name: "Quant", timeLimit: 40 },
+      { name: "VARC", timeLimit: 30 },
+      { name: "LRDI", timeLimit: 30 }
+    ]
+  },
+  {
+    name: "Bank Probationary Officer",
+    code: "BANKPO",
+    description: "Bank PO competitive exam",
+    quizCount: 60,
+    icon: "fas fa-piggy-bank",
+    colorTheme: "green",
+    sections: [
+      { name: "Quant", timeLimit: 40 },
+      { name: "VARC", timeLimit: 30 },
+      { name: "LRDI", timeLimit: 30 },
+      { name: "GK", timeLimit: 20 }
+    ]
+  },
+  {
+    name: "Staff Selection Commission CGL",
+    code: "SSCCGL",
+    description: "SSC Combined Graduate Level Exam",
+    quizCount: 60,
+    icon: "fas fa-users",
+    colorTheme: "orange",
+    sections: [
+      { name: "Quant", timeLimit: 40 },
+      { name: "VARC", timeLimit: 30 },
+      { name: "LRDI", timeLimit: 30 },
+      { name: "GK", timeLimit: 20 }
+    ]
+  },
+  {
+    name: "Bank Clerk",
+    code: "BANKCLERK",
+    description: "Bank Clerk competitive exam",
+    quizCount: 50,
+    icon: "fas fa-cash-register",
+    colorTheme: "cyan",
+    sections: [
+      { name: "Quant", timeLimit: 40 },
+      { name: "VARC", timeLimit: 30 },
+      { name: "LRDI", timeLimit: 30 }
+    ]
+  },
+  {
+    name: "Common University Entrance Test",
+    code: "CUET",
+    description: "UG entrance by NTA",
+    quizCount: 50,
+    icon: "fas fa-graduation-cap",
+    colorTheme: "red",
+    sections: [
+      { name: "Quant", timeLimit: 40 },
+      { name: "VARC", timeLimit: 30 },
+      { name: "LRDI", timeLimit: 30 }
     ]
   }
 ];
