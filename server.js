e58b463a1e19250d8736d4ba601a9e37cec0fb9a
dewboo10 <devanshusaxena8@gmail.com>
2025-07-12 21:24:13 +0530

    fix cors

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
-
 
 
 // ✅ Connect to MongoDB
